c1e37379db819213c28b62917abe6a2302c0f1ee
Vitaly Greck <vintozver@ya.ru>
2022-04-05 18:24:15 -0700

    Set authSource equals to the used database

--- modules/mongo/__init__.py
+++ modules/mongo/__init__.py
@@ -16,6 +16,7 @@ class DbSessionController(util.context.AutoRefContextItem):
         if config.db_mongo.user:
             kwargs['username'] = config.db_mongo.user
             kwargs['password'] = config.db_mongo.password
+            kwargs['authSource'] = config.db_mongo.name
         return pymongo.MongoClient(
             config.db_mongo.host, config.db_mongo.port,
             document_class=OrderedDict, tz_aware=True, uuidRepresentation='pythonLegacy',
@@ -30,6 +31,7 @@ mongoengine_connection_kwargs = dict()
 if config.db_mongo.user is not None:
     mongoengine_connection_kwargs['username'] = config.db_mongo.user
     mongoengine_connection_kwargs['password'] = config.db_mongo.password
+    mongoengine_connection_kwargs['authSource'] = config.db_mongo.name
 mongoengine_connection_kwargs['document_class'] = OrderedDict
 mongoengine_connection_kwargs['tz_aware'] = True
 mongoengine.register_connection(
