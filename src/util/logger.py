05285c8be0001bbf0899658f17daae17acd2cf67
Vitaly Greck <vintozver@ya.ru>
2026-08-30 08:01:17 +0300

    Log file in the current dir instead

--- src/util/logger.py
+++ src/util/logger.py
@@ -30,7 +30,7 @@ logging.basicConfig(
 def logger_setup():
     import logging
     logger = logging.getLogger('traceback')
-    handler = logging.FileHandler(os.path.join(os.path.join(os.getcwd(), 'run'), 'log', 'traceback_web.txt'))
+    handler = logging.FileHandler('traceback_web.txt')
     handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', '%Y-%m-%d %H:%M:%S'))
     logger.addHandler(handler)
 logger_setup()
